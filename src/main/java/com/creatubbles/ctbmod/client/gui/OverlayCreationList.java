a23e2179f1b0e09b0cb758125ae3b5ee2d115e7a
tterrag1098 <tterrag1098@gmail.com>
2015-09-25 03:34:37 -0400

    Fix thumbnail size being set to 64 again...

--- src/main/java/com/creatubbles/ctbmod/client/gui/OverlayCreationList.java
+++ src/main/java/com/creatubbles/ctbmod/client/gui/OverlayCreationList.java
@@ -97,7 +97,6 @@ public class OverlayCreationList extends Gui implements IGuiOverlay {
 	private void rebuildList() {
 		list.clear();
 		listAbsolute.clear();
-		this.thumbnailSize = 64;
 
 		gui.clearToolTips();
 

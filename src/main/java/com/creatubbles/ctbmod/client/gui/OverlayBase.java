65001b1d20ae9d2ef6dbe9fa1f5b0fd60cab0a57
tterrag1098 <tterrag1098@gmail.com>
2015-09-26 02:13:47 -0400

    Add logout/cancel buttons, implement saved users, finalize GUI layout Conflicts: 	src/main/java/com/creatubbles/ctbmod/client/gui/OverlayCreationList.java

---
+++ src/main/java/com/creatubbles/ctbmod/client/gui/OverlayBase.java
@@ -0,0 +1,94 @@
+package com.creatubbles.ctbmod.client.gui;
+
+import java.awt.Rectangle;
+
+import lombok.Getter;
+import net.minecraft.client.gui.Gui;
+
+import org.lwjgl.util.Dimension;
+
+import com.creatubbles.repack.enderlib.api.client.gui.IGuiOverlay;
+import com.creatubbles.repack.enderlib.api.client.gui.IGuiScreen;
+
+public abstract class OverlayBase extends Gui implements IGuiOverlay {
+
+	protected final int xRel, yRel;
+
+	@Getter
+	private final Dimension size;
+
+	private int xAbs, yAbs;
+
+	@Getter
+	private IGuiScreen gui;
+
+	@Getter
+	private boolean visible;
+
+	protected OverlayBase(int x, int y, Dimension dimension) {
+		this.xRel = x;
+		this.yRel = y;
+		this.size = dimension;
+	}
+
+	@Override
+	public void init(IGuiScreen screen) {
+		this.gui = screen;
+		this.xAbs = xRel + screen.getGuiLeft();
+		this.yAbs = yRel + screen.getGuiTop();
+	}
+
+	@Override
+	public final void draw(int mouseX, int mouseY, float partialTick) {
+		if (isVisible()) {
+			doDraw(mouseX, mouseY, partialTick);
+		}
+	}
+
+	protected void doDraw(int mouseX, int mouseY, float partialTick) {
+	}
+
+	@Override
+	public Rectangle getBounds() {
+		return new Rectangle(getX(), getY(), getSize().getWidth(), getSize().getHeight());
+	}
+
+	@Override
+	public boolean handleMouseInput(int x, int y, int b) {
+		return false;
+	}
+
+	@Override
+	public boolean isMouseInBounds(int mouseX, int mouseY) {
+		return getBounds().contains(mouseX, mouseY);
+	}
+	
+	@Override
+	public void setIsVisible(boolean visible) {
+		this.visible = visible;
+	}
+
+	public int getWidth() {
+		return getSize().getWidth();
+	}
+
+	public int getHeight() {
+		return getSize().getHeight();
+	}
+
+	public int getX() {
+		return xAbs;
+	}
+
+	public void setX(int x) {
+		this.xAbs = x;
+	}
+
+	public int getY() {
+		return yAbs;
+	}
+
+	public void setY(int y) {
+		this.yAbs = y;
+	}
+}
--- src/main/java/com/creatubbles/ctbmod/common/http/Creation.java
+++ src/main/java/com/creatubbles/ctbmod/common/http/Creation.java
@@ -1,10 +1,12 @@
 package com.creatubbles.ctbmod.common.http;
 
+import lombok.ToString;
 import lombok.Value;
 
 import com.google.gson.annotations.SerializedName;
 
 @Value
+@ToString(exclude = "image")
 public class Creation {
 
 	private int id;

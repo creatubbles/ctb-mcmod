cf1d1160054a81e2e38d04d48e8b3b71477723a5
tterrag1098 <tterrag1098@gmail.com>
2015-10-26 20:30:53 -0400

    Update test commands to use newest API features

--- src/main/java/com/creatubbles/ctbmod/common/command/CommandLogin.java
+++ src/main/java/com/creatubbles/ctbmod/common/command/CommandLogin.java
@@ -1,42 +1,36 @@
 package com.creatubbles.ctbmod.common.command;
 
-import javax.ws.rs.core.Response;
-
 import lombok.SneakyThrows;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 
-import com.creatubbles.api.CreatubblesAPI;
 import com.creatubbles.api.request.auth.SignInRequest;
 import com.creatubbles.api.response.auth.SignInResponse;
 import com.creatubbles.repack.endercore.common.util.ChatUtil;
 
 public class CommandLogin extends CommandBase {
 
-	public static String accessToken;
+    public static String accessToken;
 
-	@Override
-	public String getCommandName() {
-		return "ctb-login";
-	}
+    @Override
+    public String getCommandName() {
+        return "ctb-login";
+    }
 
-	@Override
-	public String getCommandUsage(ICommandSender p_71518_1_) {
-		return "/ctb-login <email> <password>";
-	}
+    @Override
+    public String getCommandUsage(ICommandSender p_71518_1_) {
+        return "/ctb-login <email> <password>";
+    }
 
-	@Override
-	@SneakyThrows
-	public void processCommand(ICommandSender p_71515_1_, String[] p_71515_2_) {
-		SignInRequest req = new SignInRequest(p_71515_2_[0], p_71515_2_[1]);
-		Response resp = req.execute();
-		if (resp.getStatus() != 200) {
-			ChatUtil.sendNoSpamClient("Login failed! Invalid email or password.");
-		} else {
-			SignInResponse access = CreatubblesAPI.GSON.fromJson(resp.readEntity(String.class), SignInResponse.class);
-			ChatUtil.sendNoSpamClient("Access Token Received: " + access.access_token);
-			accessToken = access.access_token;
-		}
-	}
-
+    @Override
+    @SneakyThrows
+    public void processCommand(ICommandSender p_71515_1_, String[] p_71515_2_) {
+        SignInResponse resp = new SignInRequest(p_71515_2_[0], p_71515_2_[1]).execute().getResponse();
+        if (resp.access_token == null) {
+            ChatUtil.sendNoSpamClient("Login failed! Invalid email or password.");
+        } else {
+            ChatUtil.sendNoSpamClient("Access Token Received: " + resp.access_token);
+            accessToken = resp.access_token;
+        }
+    }
 }

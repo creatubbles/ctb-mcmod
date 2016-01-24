919423d08cd8cffc4d5763573308c9ffe31d2387
Jevgeni Koltšin <jevgeni.koltsin@iglu.ee>
2016-01-24 18:51:47 +0200

    support "" and long creation names

--- src/main/java/com/creatubbles/ctbmod/common/command/CommandUpload.java
+++ src/main/java/com/creatubbles/ctbmod/common/command/CommandUpload.java
@@ -1,11 +1,5 @@
 package com.creatubbles.ctbmod.common.command;
 
-import java.io.File;
-import java.io.FileFilter;
-import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.Comparator;
-
 import com.creatubbles.api.request.amazon.UploadS3ImageRequest;
 import com.creatubbles.api.request.creation.CreateCreationRequest;
 import com.creatubbles.api.request.creation.CreationsUploadsRequest;
@@ -13,7 +7,6 @@ import com.creatubbles.api.request.creation.PingCreationsUploadsRequest;
 import com.creatubbles.api.response.amazon.UploadS3ImageResponse;
 import com.creatubbles.api.response.creation.CreateCreationResponse;
 import com.creatubbles.api.response.creation.CreationsUploadsResponse;
-
 import com.creatubbles.repack.endercore.common.util.ChatUtil;
 import lombok.SneakyThrows;
 import net.minecraft.client.Minecraft;
@@ -26,13 +19,19 @@ import net.minecraft.event.HoverEvent;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.ChatStyle;
 import net.minecraft.util.EnumChatFormatting;
-
 import org.apache.commons.io.filefilter.FileFilterUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class CommandUpload extends ClientCommandBase {
 
     public static final String URL_BASE = "https://www.creatubbles.com/creations/";
-    
+
     @Override
     public String getCommandName() {
         return "ctb-upload";
@@ -55,9 +54,16 @@ public class CommandUpload extends ClientCommandBase {
         }
         // create creation
         CreateCreationRequest createCreation = new CreateCreationRequest(CommandLogin.accessToken);
-        createCreation.setData("{\"name\":\"" + args[0] + "\"}");
+
+        String name = args[0];
+        try {
+            name = getCreationName(args);
+        } catch (Exception e) {
+        }
+
+        createCreation.setData("{\"name\":\"" + name + "\"}");
         CreateCreationResponse createCreationResponse = createCreation.execute().getResponse();
-        
+
         String creationID = createCreationResponse.creation.id;
 
         // create url for upload
@@ -69,9 +75,9 @@ public class CommandUpload extends ClientCommandBase {
 
         int id = 0;
         try {
-            id = Integer.valueOf(args[args.length - 1]);
+            id = getScreenshotId(args);
         } catch (Exception e) {}
-        
+
         if (id >= screenshots.length) {
             throw new NumberInvalidException("ID %d is too high! You only have %d screenshots, so ID must be between 0 and %d", id, screenshots.length, screenshots.length - 1);
         }
@@ -89,10 +95,57 @@ public class CommandUpload extends ClientCommandBase {
         PingCreationsUploadsRequest pingCreationsUploads = new PingCreationsUploadsRequest(creationsUploadsResponse.id, CommandLogin.accessToken);
         pingCreationsUploads.setData(""); // fixes null PUT error
         pingCreationsUploads.execute();
-        
+
         sender.addChatMessage(new ChatComponentText(EnumChatFormatting.GREEN.toString().concat("[Creation upload successful! (Click to view)]")).setChatStyle(new ChatStyle()
                 .setChatClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, URL_BASE + creationID))
                 .setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ChatComponentText("Click to view creation on website.")))));
+    }
+
+    private int getScreenshotId(String[] args) {
+        if (args.length == 1) {
+            return 0;
+        } else if (args[args.length - 1].endsWith("\"")) {
+            return 0;
+        } else if (args.length > 2 && args[args.length - 2].endsWith("\"") || args.length == 2) {
+            return Integer.valueOf(args[args.length - 1]);
+        }
+        return 0;
+    }
+
+    private String getCreationName(String[] args) {
+        String name;
+        if (args.length == 1) {
+            if (args[0].startsWith("\"") && args[0].endsWith("\"") && args[0].length() > 2) {
+                name = args[0].substring(1, args[0].length() - 1);
+            } else {
+                name = args[0];
+            }
+        } else {
+            if (args[0].startsWith("\"") && args[args.length - 1].endsWith("\"")) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(args[0].substring(1));
+                sb.append(StringUtils.SPACE);
+                if (args.length > 2) {
+                    sb.append(StringUtils.join(Arrays.copyOfRange(args, 1, args.length - 1), StringUtils.SPACE));
+                    sb.append(StringUtils.SPACE);
+                }
+                sb.append(args[args.length - 1].substring(0, args[args.length - 1].length() - 1));
+                name = sb.toString();
+            } else if (args[0].startsWith("\"") && args.length > 2 && args[args.length - 2].endsWith("\"")) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(args[0].substring(1));
+                sb.append(StringUtils.SPACE);
+                if (args.length > 3) {
+                    sb.append(StringUtils.join(Arrays.copyOfRange(args, 1, args.length - 2), StringUtils.SPACE));
+                    sb.append(StringUtils.SPACE);
+                }
+                sb.append(args[args.length - 2].substring(0, args[args.length - 2].length() - 1));
+                name = sb.toString();
+            } else {
+                name = args[0];
+            }
+        }
+        return name;
     }
 
     private void sortScreenshots(File[] screenshots) {

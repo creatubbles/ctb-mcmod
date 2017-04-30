a1f7ce9fdde03f4705e69068ceb774f4acf74c32
tterrag1098 <tterrag1098@gmail.com>
2017-04-29 22:43:58 -0400

    Reimplement item painting render

--- src/main/java/com/creatubbles/ctbmod/client/ClientProxy.java
+++ src/main/java/com/creatubbles/ctbmod/client/ClientProxy.java
@@ -1,34 +1,58 @@
 package com.creatubbles.ctbmod.client;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.item.Item;
-import net.minecraft.world.World;
-import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.client.registry.ClientRegistry;
-
 import com.creatubbles.ctbmod.CTBMod;
 import com.creatubbles.ctbmod.client.gui.GuiUtil;
 import com.creatubbles.ctbmod.client.render.RenderPainting;
+import com.creatubbles.ctbmod.client.render.RenderPaintingItem;
 import com.creatubbles.ctbmod.common.CommonProxy;
 import com.creatubbles.ctbmod.common.painting.PaintingHighlightHandler;
 import com.creatubbles.ctbmod.common.painting.PaintingStateMapper;
 import com.creatubbles.ctbmod.common.painting.TilePainting;
 
-public class ClientProxy extends CommonProxy {
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.model.ModelBakery;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+import net.minecraftforge.client.ForgeHooksClient;
+import net.minecraftforge.client.event.ModelBakeEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
 
+@EventBusSubscriber(Side.CLIENT)
+public class ClientProxy extends CommonProxy {
+    
+    private static RenderPaintingItem.BakedModel paintingDummyModel;
+
+    @SuppressWarnings("deprecation")
     @Override
     public void registerRenderers() {
         GuiUtil.init();
         
         ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(CTBMod.creator), 0, new ModelResourceLocation(CTBMod.DOMAIN + ":creator", "inventory"));
-        ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(CTBMod.painting), 0, new ModelResourceLocation(CTBMod.DOMAIN + ":painting", "inventory"));
+        ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(CTBMod.painting), 0, new ModelResourceLocation(CTBMod.DOMAIN + ":painting_tesr", "inventory"));
 
         ModelLoader.setCustomStateMapper(CTBMod.painting, new PaintingStateMapper());
         ClientRegistry.bindTileEntitySpecialRenderer(TilePainting.class, new RenderPainting());
         
+        RenderPaintingItem paintingItemTesr = new RenderPaintingItem();
+        ClientRegistry.bindTileEntitySpecialRenderer(RenderPaintingItem.DummyTile.class, paintingItemTesr);
+        paintingDummyModel = paintingItemTesr.baked;
+        
+        ForgeHooksClient.registerTESRItemStack(Item.getItemFromBlock(CTBMod.painting), 0, RenderPaintingItem.DummyTile.class);
+        ModelBakery.registerItemVariants(Item.getItemFromBlock(CTBMod.painting), new ModelResourceLocation(new ResourceLocation(CTBMod.DOMAIN, "painting"), "inventory"));
+
         MinecraftForge.EVENT_BUS.register(new PaintingHighlightHandler());
+    }
+    
+    @SubscribeEvent
+    public static void onModelBake(ModelBakeEvent event) {
+        event.getModelRegistry().putObject(new ModelResourceLocation(CTBMod.DOMAIN + ":painting_tesr", "inventory"), paintingDummyModel);
     }
 
     @Override
--- src/main/java/com/creatubbles/ctbmod/client/render/RenderPainting.java
+++ src/main/java/com/creatubbles/ctbmod/client/render/RenderPainting.java
@@ -2,15 +2,6 @@ package com.creatubbles.ctbmod.client.render;
 
 import java.awt.geom.Rectangle2D;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.VertexBuffer;
-import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.ResourceLocation;
-
 import org.lwjgl.opengl.GL11;
 
 import com.creatubbles.api.core.Image.ImageType;
@@ -20,6 +11,15 @@ import com.creatubbles.ctbmod.common.http.DownloadableImage;
 import com.creatubbles.ctbmod.common.painting.BlockPainting;
 import com.creatubbles.ctbmod.common.painting.TilePainting;
 import com.creatubbles.repack.endercore.client.render.IWidgetIcon;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.VertexBuffer;
+import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ResourceLocation;
 
 public class RenderPainting extends TileEntitySpecialRenderer<TilePainting> {
 
@@ -32,92 +32,101 @@ public class RenderPainting extends TileEntitySpecialRenderer<TilePainting> {
             EnumFacing facing = te.getWorld().getBlockState(te.getPos()).getValue(BlockPainting.FACING);
 
             ImageType type = te.getType();
-            int width = image.getWidth(type);
-            int height = image.getHeight(type);
-            double scaledW = image.getScaledWidth(type);
-            double scaledH = image.getScaledHeight(type);
-            ResourceLocation res = image.getResource(type);
-            
-            if (res == DownloadableImage.MISSING_TEXTURE) {
-                res = GuiUtil.Bubbles.TEXTURE;
-                scaledW = scaledH = width = height = 16;
-                GlStateManager.enableBlend();
-            }
-
-            Minecraft.getMinecraft().renderEngine.bindTexture(res);
-
-            VertexBuffer renderer = Tessellator.getInstance().getBuffer();
-
-            // TODO this code is duped between here and OverlaySelectedCreation (kinda)
-
-            Rectangle2D.Double bounds = new Rectangle2D.Double(2 / 16f, 2 / 16f, painting.getWidth() - 4 / 16f, painting.getHeight() - 4 / 16f);
-            if (width / painting.getWidth() > height / painting.getHeight()) {
-                double h = bounds.height;
-                bounds.height = bounds.getHeight() * ((double) height / width) * ((double) painting.getWidth() / painting.getHeight());
-                bounds.y += (h - bounds.getHeight()) / 2;
-            } else {
-                double w = bounds.width;
-                bounds.width = bounds.getWidth() * ((double) width / height) * ((double) painting.getHeight() / painting.getWidth());
-                bounds.x += (w - bounds.getWidth()) / 2;
-            }
-
-            double minU = 0, minV = 0;
-            double maxU = width / scaledW;
-            double maxV = height / scaledH;
-
-            // FIXME Bit of a hack for now
-            if (res == GuiUtil.Bubbles.TEXTURE) {
-                IWidgetIcon icon = GuiUtil.Bubbles.CLEAR;
-                minU = (float) icon.getX() / icon.getMap().getSize();
-                minV = (float) icon.getY() / icon.getMap().getSize();
-                maxU = minU + ((float) icon.getWidth() / icon.getMap().getSize());
-                maxV = minV + ((float) icon.getHeight() / icon.getMap().getSize());
-            }
             
             GlStateManager.pushMatrix();
             GlStateManager.translate(x, y, z);
-
-            switch (facing) {
-                case EAST:
-                    GL11.glTranslatef(0, 0, 1);
-                    GL11.glRotatef(90, 0, 1, 0);
-                    break;
-                case NORTH:
-                    GL11.glTranslatef(1, 0, 1);
-                    GL11.glRotatef(180, 0, 1, 0);
-                    break;
-                case WEST:
-                    GL11.glTranslatef(1, 0, 0);
-                    GL11.glRotatef(-90, 0, 1, 0);
-                    break;
-                default:
-                    break;
-            }
-
-            GL11.glPushAttrib(GL11.GL_COLOR_BUFFER_BIT);
             GlStateManager.disableLighting();
-            GlStateManager.doPolygonOffset(-3.0F, -1.5F);
-            GlStateManager.enablePolygonOffset();
-            renderer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX_COLOR);
-
-            float c = getColorMultiplierForFace(facing);
-            double depth = 1 / 16d;
-
-            renderer.pos(bounds.getX(), bounds.getY() + bounds.getHeight(), depth).tex(minU, minV).color(c, c, c, 1).endVertex();
-            renderer.pos(bounds.getX(), bounds.getY(), depth).tex(minU, maxV).color(c, c, c, 1).endVertex();
-            renderer.pos(bounds.getX() + bounds.getWidth(), bounds.getY(), depth).tex(maxU, maxV).color(c, c, c, 1).endVertex();
-            renderer.pos(bounds.getX() + bounds.getWidth(), bounds.getY() + bounds.getHeight(), depth).tex(maxU, minV).color(c, c, c, 1).endVertex();
-
-            Tessellator.getInstance().draw();
-            renderer.setTranslation(0, 0, 0);
-
-            GL11.glPopAttrib();
-            GlStateManager.doPolygonOffset(0.0F, 0.0F);
-            GlStateManager.disablePolygonOffset();
-            GlStateManager.disableBlend();
+            renderPaintingImage(image, type, facing, painting.getWidth(), painting.getHeight(), getColorMultiplierForFace(facing));
             GlStateManager.enableLighting();
             GlStateManager.popMatrix();
         }
+    }
+    
+    protected void renderPaintingImage(DownloadableImage image, ImageType type, EnumFacing facing) {
+        renderPaintingImage(image, type, facing, 1, 1, 1);
+    }
+    
+    protected void renderPaintingImage(DownloadableImage image, ImageType type, EnumFacing facing, int blockwidth, int blockheight, float colorMultiplier) {
+        int width = image.getWidth(type);
+        int height = image.getHeight(type);
+        double scaledW = image.getScaledWidth(type);
+        double scaledH = image.getScaledHeight(type);
+        ResourceLocation res = image.getResource(type);
+        
+        if (res == DownloadableImage.MISSING_TEXTURE) {
+            res = GuiUtil.Bubbles.TEXTURE;
+            scaledW = scaledH = width = height = 16;
+            GlStateManager.enableBlend();
+        }
+
+        Minecraft.getMinecraft().renderEngine.bindTexture(res);
+
+        VertexBuffer renderer = Tessellator.getInstance().getBuffer();
+
+        // TODO this code is duped between here and OverlaySelectedCreation (kinda)
+
+        Rectangle2D.Double bounds = new Rectangle2D.Double(2 / 16f, 2 / 16f, blockwidth - 4 / 16f, blockheight - 4 / 16f);
+        if (width / blockwidth > height / blockheight) {
+            double h = bounds.height;
+            bounds.height = bounds.getHeight() * ((double) height / width) * ((double) blockwidth / blockheight);
+            bounds.y += (h - bounds.getHeight()) / 2;
+        } else {
+            double w = bounds.width;
+            bounds.width = bounds.getWidth() * ((double) width / height) * ((double) blockheight / blockwidth);
+            bounds.x += (w - bounds.getWidth()) / 2;
+        }
+
+        double minU = 0, minV = 0;
+        double maxU = width / scaledW;
+        double maxV = height / scaledH;
+
+        // FIXME Bit of a hack for now
+        if (res == GuiUtil.Bubbles.TEXTURE) {
+            IWidgetIcon icon = GuiUtil.Bubbles.CLEAR;
+            minU = (float) icon.getX() / icon.getMap().getSize();
+            minV = (float) icon.getY() / icon.getMap().getSize();
+            maxU = minU + ((float) icon.getWidth() / icon.getMap().getSize());
+            maxV = minV + ((float) icon.getHeight() / icon.getMap().getSize());
+        }
+        
+        GlStateManager.pushMatrix();
+
+        switch (facing) {
+            case EAST:
+                GlStateManager.translate(0, 0, 1);
+                GlStateManager.rotate(90, 0, 1, 0);
+                break;
+            case NORTH:
+                GlStateManager.translate(1, 0, 1);
+                GlStateManager.rotate(180, 0, 1, 0);
+                break;
+            case WEST:
+                GlStateManager.translate(1, 0, 0);
+                GlStateManager.rotate(-90, 0, 1, 0);
+                break;
+            default:
+                break;
+        }
+
+        GlStateManager.doPolygonOffset(-3.0F, -1.5F);
+        GlStateManager.enablePolygonOffset();
+        renderer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX_COLOR_NORMAL);
+
+        float c = colorMultiplier;
+        double depth = 1 / 16d;
+
+        renderer.pos(bounds.getX(), bounds.getY() + bounds.getHeight(), depth).tex(minU, minV).color(c, c, c, 1).normal(0, 0, 1).endVertex();
+        renderer.pos(bounds.getX(), bounds.getY(), depth).tex(minU, maxV).color(c, c, c, 1).normal(0, 0, 1).endVertex();
+        renderer.pos(bounds.getX() + bounds.getWidth(), bounds.getY(), depth).tex(maxU, maxV).color(c, c, c, 1).normal(0, 0, 1).endVertex();
+        renderer.pos(bounds.getX() + bounds.getWidth(), bounds.getY() + bounds.getHeight(), depth).tex(maxU, minV).color(c, c, c, 1).normal(0, 0, 1).endVertex();
+
+        Tessellator.getInstance().draw();
+        renderer.setTranslation(0, 0, 0);
+
+        GlStateManager.doPolygonOffset(0.0F, 0.0F);
+        GlStateManager.disablePolygonOffset();
+        GlStateManager.disableBlend();
+        GlStateManager.popMatrix();
     }
 
     private static float getColorMultiplierForFace(EnumFacing face) {

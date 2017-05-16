108110dab0a1b3ccd2b2a50806b70ad179bce2e6
tterrag1098 <tterrag1098@gmail.com>
2017-05-15 21:44:36 -0400

    Double the render distance of paintings

--- src/main/java/com/creatubbles/ctbmod/common/painting/TilePainting.java
+++ src/main/java/com/creatubbles/ctbmod/common/painting/TilePainting.java
@@ -66,6 +66,11 @@ public class TilePainting extends TileEntityBase {
     public AxisAlignedBB getRenderBoundingBox() {
         return BlockPainting.getCompleteBoundingBox(getWorld(), getPos());
     }
+    
+    @Override
+    public double getMaxRenderDistanceSquared() {
+        return super.getMaxRenderDistanceSquared() * 4; // Twice as far as default
+    }
 
     @Override
     protected void writeCustomNBT(NBTTagCompound root) {

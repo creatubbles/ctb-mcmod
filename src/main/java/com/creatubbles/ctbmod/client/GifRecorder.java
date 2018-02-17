fad9a43dc2c986f0cb7cea687d736940671a81cd
tterrag <tterrag1098@gmail.com>
2018-02-16 23:13:50 -0500

    Optimize gif recording: stream to disk during record, reduce frame count

--- src/main/java/com/creatubbles/ctbmod/client/GifRecorder.java
+++ src/main/java/com/creatubbles/ctbmod/client/GifRecorder.java
@@ -7,7 +7,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.nio.IntBuffer;
-import java.util.List;
+import java.util.Queue;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -23,9 +23,10 @@ import com.creatubbles.ctbmod.CTBMod;
 import com.creatubbles.repack.dragon66.AnimatedGIFWriter;
 
 import jersey.repackaged.com.google.common.collect.Lists;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.SneakyThrows;
+import lombok.experimental.Accessors;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.OpenGlHelper;
@@ -40,6 +41,7 @@ import net.minecraft.util.text.event.ClickEvent;
 import net.minecraft.util.text.event.HoverEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.Phase;
 
 /** Most code taken from ScreenShotHelper with the File IO code replaced */
 public class GifRecorder {
@@ -47,12 +49,13 @@ public class GifRecorder {
     @RequiredArgsConstructor
     private static class GifWriteTask implements Callable<File> {
         
-        private final List<int[]> frames;
+        private final Queue<int[]> frames;
         private final Framebuffer buffer;
         private final int width, height;
         
-        @Getter
-        private float percentComplete;
+        @Setter
+        @Accessors(fluent = true)
+        private boolean finished;
         
         @Override
         public File call() throws Exception {
@@ -70,44 +73,50 @@ public class GifRecorder {
                 try {
                     writer.prepareForWrite(os, -1, -1);
                     
-                    float percPerFrame = 1.0f / frames.size();
-
-                    for (int[] frame : frames) {
-                        BufferedImage bufferedimage = null;
-
-                        if (OpenGlHelper.isFramebufferEnabled()) {
-                            bufferedimage = new BufferedImage(buffer.framebufferWidth, buffer.framebufferHeight, 1);
-                            int j = buffer.framebufferTextureHeight - buffer.framebufferHeight;
-
-                            for (int k = j; k < buffer.framebufferTextureHeight; ++k) {
-                                for (int l = 0; l < buffer.framebufferWidth; ++l) {
-                                    bufferedimage.setRGB(l, k - j, frame[k * buffer.framebufferTextureWidth + l]);
-                                }
+                    while (!finished || !frames.isEmpty()) {
+                        int[] frame = frames.poll();
+                        if (frame == null) {
+                            try {
+                                Thread.sleep(50);
+                            } catch (InterruptedException e) {
+                                break;
                             }
                         } else {
-                            bufferedimage = new BufferedImage(width, height, 1);
-                            bufferedimage.setRGB(0, 0, width, height, frame, 0, width);
+
+                            // Let's add some inter-frame sleep to avoid CPU spiking and disk thrashing
+                            // This makes the output a bit slower but results in a more stable FPS during recording
+                            Thread.sleep(25);
+
+                            BufferedImage bufferedimage = null;
+    
+                            if (OpenGlHelper.isFramebufferEnabled()) {
+                                bufferedimage = new BufferedImage(buffer.framebufferWidth, buffer.framebufferHeight, 1);
+                                bufferedimage.setRGB(0, 0, buffer.framebufferTextureWidth, buffer.framebufferTextureHeight, frame, 0, buffer.framebufferTextureWidth);
+                            } else {
+                                bufferedimage = new BufferedImage(width, height, 1);
+                                bufferedimage.setRGB(0, 0, width, height, frame, 0, width);
+                            }
+                            
+                            float scale;
+                            if (width > height) {
+                                scale = 500.0f / width;
+                            } else {
+                                scale = 500.0f / height;
+                            }
+                            if (scale < 1) {
+                                int w = (int) (width * scale);
+                                int h = (int) (height * scale);
+                                BufferedImage resizedImg = new BufferedImage(w, h, bufferedimage.getType());
+                                Graphics2D g2 = resizedImg.createGraphics();
+                                g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+                                g2.drawImage(bufferedimage, 0, 0, w, h, null);
+                                g2.dispose();
+                                bufferedimage = resizedImg;
+                            }
+                            
+                            writer.writeFrame(os, bufferedimage, 50);
+                            framesProcessed++;
                         }
-                        
-                        float scale;
-                        if (width > height) {
-                            scale = 500.0f / width;
-                        } else {
-                            scale = 500.0f / height;
-                        }
-                        if (scale < 1) {
-                            int w = (int) (width * scale);
-                            int h = (int) (height * scale);
-                            BufferedImage resizedImg = new BufferedImage(w, h, bufferedimage.getType());
-                            Graphics2D g2 = resizedImg.createGraphics();
-                            g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-                            g2.drawImage(bufferedimage, 0, 0, w, h, null);
-                            g2.dispose();
-                            bufferedimage = resizedImg;
-                        }
-                        
-                        writer.writeFrame(os, bufferedimage, 30);
-                        percentComplete = Math.min(1, percentComplete + percPerFrame);
                     }
 
                     writer.finishWrite(os);
@@ -132,18 +141,23 @@ public class GifRecorder {
     /** A buffer to hold pixel values returned by OpenGL. */
     private static IntBuffer pixelBuffer;
 
-    private static List<int[]> frames = Lists.newArrayList();
+    private static Queue<int[]> frames = Lists.newLinkedList();
 
     private static final ExecutorService executor = Executors.newSingleThreadExecutor();
     private static Future<File> future = null;
     private static GifWriteTask task = null;
     
     private static final int MAX_STEPS = 30;
-    private static int lastStep = 0;
+    
+    private static int framesRecorded, framesProcessed;
+    private static int lastStep;
 
     @SubscribeEvent
     @SneakyThrows
     public void onClientTick(ClientTickEvent event) {
+        if (event.phase == Phase.START) {
+            return;
+        }
 
         final Framebuffer buffer = Minecraft.getMinecraft().getFramebuffer();
 
@@ -179,10 +193,15 @@ public class GifRecorder {
             pixelBuffer.get(pixelValues);
             TextureUtil.processPixelValues(pixelValues, width, height);
             frames.add(pixelValues);
-
+            framesRecorded++;
+            
+            if (task == null) {
+                future = executor.submit(task = new GifWriteTask(frames, buffer, width, height));
+            }
         } else if (task != null) {
-            StringBuffer sb = new StringBuffer("Writing Gif ");
-            int step = MathHelper.ceiling_float_int(MAX_STEPS * task.getPercentComplete());
+            task.finished(true);
+            StringBuffer sb = new StringBuffer("Writing Gif: ");
+            int step = MathHelper.ceiling_float_int(MAX_STEPS * ((float) framesProcessed / framesRecorded));
             if (step != lastStep) {
                 sb.append("[");
                 for (int i = 0; i < step; i++) {
@@ -210,10 +229,8 @@ public class GifRecorder {
                 Minecraft.getMinecraft().ingameGUI.getChatGUI().printChatMessageWithOptionalDeletion(msg, task.hashCode());
                 future = null;
                 task = null;
-                lastStep = 0;
+                framesRecorded = framesProcessed = lastStep = 0;
             }
-        } else if (!frames.isEmpty()) {
-            future = executor.submit(task = new GifWriteTask(frames, buffer, width, height));
         }
     }
 }
